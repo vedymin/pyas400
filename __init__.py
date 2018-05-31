c123a9d5fcee17006b1036e926d08705fc571be3
vedymin <39482545+vedymin@users.noreply.github.com>
2018-05-31 15:32:50 +0200

    a lot...

--- __init__.py
+++ __init__.py
@@ -8,8 +8,9 @@ class ConnectionManager:
 
     def __init__(self):
         self.PCommConnMgr = win32com.client.Dispatch("PCOMM.autECLConnMgr")
-        # self.OIA = win32com.client.Dispatch("PCOMM.autECLOIAObj")
+
         self.connList = self.PCommConnMgr.autECLConnList
+        self.OIA = win32com.client.Dispatch("PCOMM.autECLOIA")
 
         self.activeSession = None
         self.sessions = {}
@@ -28,11 +29,22 @@ class ConnectionManager:
         if self.get_text(1, 36, 7, connection_name=session) == "Sign On":
             return False
         else:
-            return True
+            if self.get_text(1, 2, 7, connection_name=session) == "MENUINI":
+                self.send_keys("1")
+                self.enter()
+                self.enter()
+                return True
+            elif self.get_text(23, 17, 6, connection_name=session) == "HARDIS":
+                self.enter()
+                return True
+            else:
+                return True
 
     def open_session(self, connection_name):
         _session = win32com.client.Dispatch("PCOMM.autECLSession")
         _session.SetConnectionByName(connection_name)
+
+        self.OIA.SetConnectionByName(connection_name)
 
         if not _session.Ready:
             _session.StartCommunication()
@@ -50,29 +62,83 @@ class ConnectionManager:
             temp_session = self.sessions[connection_name]
 
         if length is None:
+            self.OIA.WaitForInputReady()
             temp_session.autECLPS.autECLFieldList.Refresh()
             field = temp_session.autECLPS.autECLFieldList.FindFieldByRowCol(row, col)
             length = field.Length
 
+        self.OIA.WaitForInputReady()
         result = temp_session.autECLPS.GetText(row, col, length)
         return result
 
     def send_keys(self, key, row=None, col=None, connection_name=None):
+        self.OIA.WaitForInputReady()
+
         temp_session = self.activeSession
         if connection_name is not None:
             temp_session = self.sessions[connection_name]
 
         if row is None or col is None:
+            self.OIA.WaitForInputReady()
             temp_session.autECLPS.SendKeys("%s" % key)
+            self.OIA.WaitForInputReady()
 
         else:
+            self.OIA.WaitForInputReady()
+            self.tab()
+            self.OIA.WaitForInputReady()
             temp_session.autECLPS.SendKeys("%s" % key, row, col)
-
+            self.OIA.WaitForInputReady()
 
     def enter(self):
+        self.OIA.WaitForInputReady()
         self.send_keys("[enter]")
+        self.OIA.WaitForInputReady()
 
+    def set_cursor(self, row, col, connection_name=None):
+        self.OIA.WaitForInputReady()
 
+        temp_session = self.activeSession
+        if connection_name is not None:
+            temp_session = self.sessions[connection_name]
+
+        self.OIA.WaitForInputReady()
+        temp_session.autECLPS.SetCursorPos(row, col)
+        self.OIA.WaitForInputReady()
+
+    def tab(self, count=1, connection_name=None):
+        self.OIA.WaitForInputReady()
+        temp_session = self.activeSession
+        if connection_name is not None:
+            temp_session = self.sessions[connection_name]
+
+        for n in range(count):
+            self.OIA.WaitForInputReady()
+            self.send_keys("[tab]")
+            self.OIA.WaitForInputReady()
+
+    def fkey(self, key, count=1, connection_name=None):
+        self.OIA.WaitForInputReady()
+        temp_session = self.activeSession
+        if connection_name is not None:
+            temp_session = self.sessions[connection_name]
+
+        for n in range(count):
+            self.OIA.WaitForInputReady()
+            self.send_keys("[pf{}]".format(key))
+            self.OIA.WaitForInputReady()
+
+    def esc(self, count=1, connection_name=None):
+        self.OIA.WaitForInputReady()
+        temp_session = self.activeSession
+        if connection_name is not None:
+            temp_session = self.sessions[connection_name]
+
+        for n in range(count):
+            self.OIA.WaitForInputReady()
+            self.send_keys("[attn]")
+            time.sleep(1)
+            self.OIA.WaitForInputReady()
 
 # conn = ConnectionManager()
 # d = conn.get_available_connections()
